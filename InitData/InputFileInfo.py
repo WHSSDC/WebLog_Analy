054970f9306b1de952b1272d0eb28d44a8a9f25e
BlackAngle <blackangle0929@163.com>
2018-02-05 00:26:00 +0800

    # Add CLI logpath and outpath v1

--- InitData/InputFileInfo.py
+++ InitData/InputFileInfo.py
@@ -2,16 +2,16 @@
 import  time
 import os
 # 获取当前路径，若日志路径为空，则自动搜索目录下的WebLog目录；
-PATH =  os.getcwd()
 
 
 # 日志分析类
 class InputFileInfo :
     # 存储对象名等于文件名+时间戳
-
-    def __init__(self,logPath,):
-        if logPath is None :
-            logPath = PATH
+    def __init__(self,logPath,outPath):
+        # 从输入当中获取 日志路径
+        logPath
+        # 从输入当中获取 分析报告输出路径
+        outPath
     # 获取名字
     def setJobName(self):
         jobName = None
--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import click
 import sys
-
+import InitData.InputFileInfo
 import os
 osPATH =  os.getcwd()
+
+# CLI功能实现，所有输出经过过滤后传输到各实例中
+# 还没有添加logging 记录器进行异常处理
 @click.command()
-@click.option('--logpath', default=osPATH, help='WEB日志文件目录.')
-@click.option('--outpath', default=osPATH, help='分析报告输出目录.')
+@click.option('--logpath', default=osPATH, help='WEB日志文件目录,默认值是当前目录.')
+@click.option('--outpath', default=osPATH, help='分析报告输出目录,默认值是当前目录.')
 def getLogPath(logpath,outpath):
-    print(logpath)
-    print(outpath)
-    return True
-
-
+    if logpath is None and outpath is None :
+        raise ValueError
+    # 这里会向 InputFileInfo 类传参数用于帮助初始化
 
 if __name__ == '__main__':
     getLogPath()
