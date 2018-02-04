de07c68da93003593ff02a08a0cc693a527f1f87
BlackAngle <blackangle0929@163.com>
2018-02-05 00:12:17 +0800

    # Add CLI logpath and outpath

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import click
 import sys
 
 import os
-
+osPATH =  os.getcwd()
 @click.command()
-@click.option('-p', default=1, help='Number of greetings.')
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--count', default=1, help='Number of greetings.')
-def getLogPath():
-    pass
+@click.option('--logpath', default=osPATH, help='WEB日志文件目录.')
+@click.option('--outpath', default=osPATH, help='分析报告输出目录.')
+def getLogPath(logpath,outpath):
+    print(logpath)
+    print(outpath)
+    return True
 
 
 
 if __name__ == '__main__':
-    pass
+    getLogPath()
